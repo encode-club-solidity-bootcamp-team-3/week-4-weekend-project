d36dcabcf8c16a9a320ff559ecf4b4b53caa5c9b
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-08-20 09:33:15 +0200

    Update app.service.ts

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,40 +1,89 @@
 import { Injectable } from '@nestjs/common';
-import { MyToken } from './assets/typechain-types/contracts/ERC20Votes.sol/MyToken';
-import { MyToken__factory } from './assets/typechain-types/factories/contracts/ERC20Votes.sol/MyToken__factory';
 import { ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json';
+import * as tokenBallotJson from './assets/TokenizedBallot.json';
 
-// const CONTRACT_ADDRESS = '0x208F75C3A395Ad125D0D641D9a2648F837a58538';
-const CONTRACT_ADDRESS = '0x64dc27fe475A9408dA8563800CBb3725C9B81694';
+
+const CONTRACT_ADDRESS = '0x208F75C3A395Ad125D0D641D9a2648F837a58538';
+const BALLOT_ADDRESS = '0x208F75C3A395Ad125D0D641D9a2648F837a58538';
+//0x8a41e75fca1427d313a75de58c3739873e7175c3
+//0x208F75C3A395Ad125D0D641D9a2648F837a58538
+//ballot contract: 0x83611D5d1E4efc8c74B86090aA9CD304889d854a
 
 @Injectable()
 export class AppService {
-  contract: MyToken;
-  provider: ethers.JsonRpcProvider;
+
+  contract: ethers.Contract;
+  contract_ballot: ethers.Contract;
+  provider: ethers.Provider;
   wallet: ethers.Wallet;
+  private proposals: string[] = []; //
+
 
   constructor() {
-    this.provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL);
-    this.wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, this.provider);
-    this.contract = MyToken__factory.connect(CONTRACT_ADDRESS, this.wallet);
+    const provider = new ethers.JsonRpcProvider(
+        process.env.RPC_ENDPOINT_URL ?? '',
+    )
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? '', provider);
+    this.contract = new ethers.Contract(CONTRACT_ADDRESS, tokenJson.abi, wallet);
+    this.contract_ballot = new ethers.Contract(BALLOT_ADDRESS, tokenBallotJson.abi, wallet);
+  }
+
+
+  getContractAddress():{address: string} {
+    return {address: CONTRACT_ADDRESS};
+  }
+
+  getTotalSupply() {
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? '',);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? '', provider);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, tokenJson.abi, wallet);
+    return contract.totalSupply();
+  }
+
+  getTokenBalance(address: string) {
+    return this.contract.balanceOf(address);
+
+
+  }
+
+  async mintTokens(address: string): Promise <any>{
+    console.log("Minting tx to " + address)
+    const tx = await this.contract.mint(address, ethers.parseUnits("1"));
+    const receipt = await tx.wait(); 
+    //return {result: true};
+    return {success: true, txHash: '...'};
+
+  }
+
+  async selfDelegate(address: string): Promise <any>{
+    console.log("Self delegating to " + address)
+    const tx = await this.contract.delegate(address);
+    const receipt = await tx.wait(); 
+    //return {result: true};
+    return {success: true, txHash: '...'};
+
   }
 
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello noWorld!';
   }
   getAnotherThing(): string {
-    return 'another thing';
+    return 'Hello other thing!';
   }
-  getContractAddress(): { address: string } {
-    return { address: CONTRACT_ADDRESS };
+  async createNewProposal(proposal: string): Promise <any>{
+    console.log("New proposal " + proposal)
+    this.proposals.push(proposal)
+    //return {result: true};
+    return {success: true, txHash: '...'};
+
   }
-  getTotalSupply() {
-    return this.contract.totalSupply();
+  //createNewProposal(proposal: string): void { //
+   // this.proposals.push(proposal);//
+  //}
+  
+  getProposals(): string[] {//
+    return this.proposals;//
   }
-  getTokenBalance(address: string) {
-    return this.contract.balanceOf(address);
-  }
-  async mintTokens(address: string, amount: number) {
-    const tx = await this.contract.mint(address, amount);
-    return await tx.wait();
-  }
+
 }
